69b2e98dd0370f76125f0f17a39ada58c78d7978
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-09-28 22:09:54 +0100

    Refactor Prisma client initialization to use a singleton pattern

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient({
-  log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-});
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-export const prisma = globalForPrisma.prisma ?? client;
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
