7781886d5c37096eaaa739916567adbc8d22672c
Nikola Zjalic <nzjalic@gmail.com>
2024-09-29 22:52:35 -0400

    fix landing page redirect after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Footer from "@/components/footer";
 import FeaturesSection from "@/components/landing-page/features-section";
 import HeroSection from "@/components/landing-page/hero-section";
 import PricingSection from "@/components/landing-page/pricing-section";
 import SubscribeSection from "@/components/landing-page/subscribe-section";
+import { useSession } from "@/providers/session-provider";
+import { redirect } from "next/navigation";
 
 export default function LandingPage() {
+  const { user } = useSession();
+  if (user) redirect("/dashboard");
+
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <main className="flex-1">
