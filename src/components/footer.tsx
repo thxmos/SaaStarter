de1b267fb05b31761648e6189f60b9806af9ed1e
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-10-06 00:47:47 +0100

    Refactor footer component classnames

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { APP_NAME } from "@/constants";
 const Footer = () => {
   return (
     <footer
-      className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t justify-center"
+      className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 justify-center"
       role="contentinfo"
       aria-label="Site footer"
     >
