4dd05a83b9962fa64e6f505f7712f83163dc58ad
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-10-06 02:22:25 +0100

    Refactor: Redirect to dashboard home page in Dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,8 @@
 import ProtectedLayout from "@/components/protected-layout";
 import BeanMachineTab from "./home/bean-machine-tab";
+import { redirect } from "next/navigation";
 
 export default function Dashboard({}) {
-  return (
-    <ProtectedLayout redirectUrl="/auth">
-      <main className="flex-1 p-8 overflow-y-auto bg-background">
-        <div className="w-full max-w-5xl">
-          <BeanMachineTab />
-        </div>
-      </main>
-    </ProtectedLayout>
-  );
+  redirect("/dashboard/home");
+  return <></>;
 }
